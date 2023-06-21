1c5a9a951cfb802ff2d427a7821b9fa1c3ec17c8
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-06-22 02:13:55 +0400

    add form for new opinion

--- opinions_app.py
+++ opinions_app.py
@@ -5,7 +5,11 @@ from random import randrange
 
 # из модуля flask импортируем класс Flask
 # также импортируем функцию render_template
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for, flash
+# новые импорты для создания форм
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
 
 # из модуля (расширение для flask) импортируем нужный класс
 from flask_sqlalchemy import SQLAlchemy
@@ -17,8 +21,13 @@ app = Flask(__name__)
 # для этого добавляем в настройки проекта новый ключ
 # и задаем ему значение
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
 # Задаётся конкретное значение для конфигурационного ключа
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# для всех форм включена поддержка защиты с использованием CSRF-токена
+# такая защита требует секретного ключа из настроек Flask
+app.config['SECRET_KEY'] = 'MY SECRET KEY FOR FORM'
 
 # создаем экземпляр класса SQLAlchemy и в него передается
 # в кач-ве параметра экземпляр приложения Flask
@@ -32,6 +41,44 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
+# класс формы опишем сразу после модели Opinion
+class OpinionForm(FlaskForm):
+    # форма должна состоять из 3-ех полей - title, text, source
+    # у каждого поля свой тип, который мы импортируем из модуля wtform
+    # это возможно благодаря интеграции flask и  wtform
+
+    # для каждого поля есть свой тип - например StringField
+    # это значит создано поле строкового типа
+    # для него указано 2 параметра - lable и  validators
+    # lable - название поля, которое обычно отображается рядом с полем
+    # или внутри этого поля
+    # validators - список валидаторов, которые выполняет проверку
+    # на то, что поля соответствуют ожидаемому результату
+    # валидатор DataRequired проверяет наличие данных в поле ввода
+    # валидатор Length проверяет длину строки
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+
+    # для поля text также используется валидатор DataRequired
+    # который проверяет наличие данных в поле ввода
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+
+    # валидатор Length проверяет длину строки
+    # валидатор Optional позволяет полю source быть необязательным
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+
+    submit = SubmitField('Добавить')
 
 
 # функция с основной логикой обернутая в декоратор app.route,
@@ -56,10 +103,56 @@ def index_view():
 
 
 # добавим новую функцию для добавления нового мнения о фильме
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    return render_template('add_opinion.html')
-    # return 'Совсем скоро тут будет случайное мнение о фильме!'
+    # создаем экземпляр формы
+    form = OpinionForm()
+    # если ошибок не возникло, то
+    if form.validate_on_submit():
+        text = form.text.data
+        # если в БД уже есть мнение о фильме с таким текстом, который ввел
+        # пользователь, то:
+        # (first()) вернет первую запись соответсвующего запроса
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # вызовем функцию flash и передаем соответствующее сообщ-е
+            flash('Такое мнение уже было оставлено ранее!')
+            # после чего вернем пользователя на страницу с формой
+            # "Добавить новое мнение"
+            return render_template('add_opinion.html', form=form)
+
+        # если же такого мнения еще не было, создаем его
+        # нужно создать новый экземпляр класса Opinion
+        # новый экземпляр модели, новое мнение о фильме
+        # каждое поле модели = соответствующим данным, которые
+        # пользователь отправил через форму
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+
+        # затем добавляем этот экземпляр класса Opinion посредством сессии
+        # добавляется новая запись мнения о фильме в БД
+        db.session.add(opinion)
+        # зафиксируем изменения в БД
+        db.session.commit()
+        # затем перейдем на страницу добавленного мнения
+        return redirect(url_for('opinion_view', id=opinion.id))
+    # иначе просто отрисовать страницу с формой
+    return render_template('add_opinion.html', form=form)
+
+
+# добавим новую функцию, обернутую в декоратор @app.route
+# который свяжет данную функцию с указанным URL
+# указываем конвертер пути для id конкретного мнения
+@app.route('/opinions/<int:id>')
+# сама функция. В кач-ве параметра она принимает id мнения
+def opinion_view(id):
+    # запрашиваем конкретное мнение по id
+    opinion = Opinion.query.get_or_404(id)
+    # передаем это мнение в шаблон с помощью функции
+    # render_template
+    return render_template('opinion.html', opinion=opinion)
 
 
 # в этом блоке команда запуска приложения
